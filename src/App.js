2e7b43e1ee0852f837ef1516c9d632f9a48faca6
홍요한 <ghddygks45@gmail.com>
2026-01-18 23:56:47 +0900

    feat: 리엑트 쿼리 개발자도구 설치 및 리엑트 쿼리 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// React Query의 핵심 객체(QueryClient)와
+// 전역으로 쿼리 기능을 제공하는 Provider
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import HomePage from "./HomePage";
+
+// QueryClient 인스턴스 생성
+// → 서버 상태 캐시, 요청 관리, 리트라이, staleTime 등을 관리하는 "중앙 관리자"
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // QueryClientProvider로 감싸면
+    // 이 안에 있는 모든 컴포넌트에서 React Query 사용 가능
+    <QueryClientProvider client={queryClient}>
+      <HomePage />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 

8ecf883f6024b5656024bbfe2d748ba01d34f357
홍요한 <ghddygks45@gmail.com>
2026-01-19 02:34:17 +0900

    feat: 쿼리 함수 나누고, 해당 페이지에서는 해당 쿼리값만 사용하게 하기

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "./Navigation";
+import styles from "./App.module.css";
+import "./App.font.css";
+
+function App() {
+  return (
+    <>
+      <Navigation className={styles.nav} />
+      <div className={styles.body}>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default App;
